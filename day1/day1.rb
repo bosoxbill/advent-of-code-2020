ed0768bf09d404df0603f7d7b259c9b0772a493f
Bill Desmarais <bill@wellist.com>
2020-12-01 11:02:05 -0500

    day1pt2

--- day1/day1.rb
+++ day1/day1.rb
@@ -6,11 +6,13 @@ File.open('input.txt', 'r') do |f|
   end
 end
 
-h.keys.each do |k|
-  val = 2020 - k
-  if h[val]
-    out = k * h[val]
-    puts "Output: #{out}"
-    exit
+h.keys.each do |k1|
+  h.keys.each do |k2|
+    val = 2020 - k1 - k2
+    if h[val]
+      out = k1 * k2 * h[val]
+      puts "Output: #{out}"
+      exit
+    end
   end
 end
